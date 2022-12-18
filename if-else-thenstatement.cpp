3cdc7aa8c6e5a6e227411acc951f664d8b8b2e52
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-12-18 20:33:47 +0530

    Create if-else-thenstatement.cpp

---
+++ if-else-thenstatement.cpp
@@ -0,0 +1,15 @@
+Module Module1
+   Sub Main()
+   {
+       if(a>b) then
+        console.WriteLine("a is greater than b")
+         Else
+         console.WriteLine("a is not greater than b")
+         
+         endif
+         console.WriteLine("value of a is : {0}",a)
+         
+         Sub End
+         
+         End Module
+         }
